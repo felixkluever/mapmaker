20d2b5744550632433e6aaac1a9208c669455bc2
Felix Klüver <privat@felix-kluever.de>
2023-11-29 20:02:18 +0100

    multi file system working

---
+++ src/demo.cpp
@@ -0,0 +1,5 @@
+#include "../include/demo.h"
+
+int calc(int a, int b) {
+    return a+b;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-//#include "include/demo.h"
+#include "../include/demo.h"
 
 int main ( int argc, char* argv[]) {
     std::cout << "Hello world" << std::endl;
 
-    //std::cout << calc(3,4) << std::endl;
+    std::cout << calc(3,4) << std::endl;
 }
