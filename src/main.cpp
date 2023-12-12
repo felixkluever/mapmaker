92751aad6419d09f817f5141fd652a286d685c1a
Felix Klüver <privat@felix-kluever.de>
2023-12-12 10:32:02 +0100

    fix: non creation bug

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,26 @@
 #include "../include/perlin_noise.h"
 
 int main ( int argc, char* argv[]) {
-    const int width = 640;
-    const int height = 480;
+    const int width = 1920;
+    const int height = 1080;
 
-    Image image = Image(width, height);
-
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            image.SetColor(Color((float)x / (float)width, 1.0f - ((float)x / (float)width), (float)y / (float)height), x, y);
-        }
-    }
-
-    image.Export("image.bmp");
-
-    Perlin_Noise noise = Perlin_Noise(63385686597);
+    Perlin_Noise noise = Perlin_Noise(5686597);
     Image perlinnoise = Image(width, height);
 
     for (int y = 0; y < height; y++)
     {
         for (int x = 0; x < width; x++)
-        {
-            std::cout << noise.noise(x,y);
-            Color color = Color(noise.noise(x, y, 0), noise.noise(x, y, 0), noise.noise(x, y, 0));
+        {            
+            double X = (double)x/((double)width);
+			double Y = (double)y/((double)height);
+
+            //function to play around with
+            // factor in front >1 makes harder lines
+            // factor in front <1 controlls brightness
+
+            double n = noise.noise(10*X, 10*Y, 0.8);
+
+            Color color = Color(n, n, n);
             perlinnoise.SetColor(color, x, y);
         }
         
