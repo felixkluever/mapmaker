a1b4994d307e75205f322a31fe1592d581b6d079
Felix Klüver <privat@felix-kluever.de>
2023-12-14 23:04:01 +0100

    add: map class

---
+++ include/map.h
@@ -0,0 +1,16 @@
+#include "image.h"
+
+class Map {
+public:
+    Map();
+private:
+
+    //terrain
+    double sea_level;
+    double snow_level;
+
+    //colours
+    //static const  Color sea_colour = Color(0, 0, 1);
+    //static const Color snow_colour = Color(1, 1, 1);
+    //static Color grass_colour;  
+};
