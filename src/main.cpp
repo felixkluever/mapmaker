275684de58aafb25fe0af7512addd60db9c48b7b
Felix Klüver <privat@felix-kluever.de>
2024-10-05 17:22:12 +0200

    add: window

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,9 @@
 #include "../include/perlin_noise.h"
 #include "../include/map.h"
 
-int main ( int argc, char* argv[]) {
-    const int width = 1920;
-    const int height = 1080;
-    
+#include <SFML/Graphics.hpp>
+
+void drawmap(int const width, int const height){
     int seed = 1;
     //creates a random seed
     srand((unsigned) time(NULL));
@@ -25,4 +24,36 @@ int main ( int argc, char* argv[]) {
     }
 
     perlinnoise.Export("map.bmp");
+}
+
+int main ( int argc, char* argv[]) {
+    const int width = 1920;
+    const int height = 1080;
+    
+    drawmap(width, height);
+    // window setup
+    sf::RenderWindow window(sf::VideoMode(width, height), "mapmaker");    
+    
+    //window loop
+    while (window.isOpen()){
+        //events
+        sf::Event event;
+        while (window.pollEvent(event)){
+            if (event.type == sf::Event::Closed) window.close();
+        }
+        drawmap(width, height);
+
+        sf::Texture map;
+        if (!map.loadFromFile("map.bmp"))
+        {
+            return EXIT_FAILURE;
+        }
+        sf::Sprite sprite (map);
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
 } 
